618f5a52cf18561de38d37e2bcd2f555424f101b
Allan Tatter <allan.tatter@gmail.com>
2018-02-23 17:12:37 +0200

    Update readme

--- src/app.js
+++ src/app.js
@@ -20,33 +20,6 @@ import HMKit from 'hmkit';
 // PASTE CLIENT CERTIFICATE SNIPPET HERE
 
 async function app() {
-  /*
-
-   Before using Telematics in HMKit, you must get the Access Certificate for the car / emulator:
-   - go to https://developers.high-mobility.com
-   - LOGIN
-   - go to Tutorials ›› SDK ›› Node.js for instructions to connect a service to the car
-   - find and do the tutorial for connecting a Service to the car
-   - authorise the service
-   - take a good look into the mirror, you badass
-   - open the SANDBOX car emulator
-   - on the left, in the Authorised Services list, choose the Service you used before
-   - copy the ACCESS TOKEN
-   - paste it below to the appropriately named variable
-
-   Bonus steps again:
-   - get a beverage
-   - quench your thirst
-   - change the world with your mind
-   - explore the APIs
-
-
-   An example of an access token:
-
-   awb4oQwMHxomS926XHyqdx1d9nYLYs94GvlJYQCblbP_wt-aBrpNpmSFf2qvhj18GWXXQ-aAtSaa4rnwBAHs5wpe1aK-3bD4xfQ3qtOS1QNV3a3iJVg03lTdNOLjFxlIOA
-
-   */
-
   const accessCertificate = await hmkit.downloadAccessCertificate(
     "PASTE ACCESS TOKEN HERE"
   );
